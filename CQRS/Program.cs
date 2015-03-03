ac2dd3950888e2c58630cea578eeb99ce471144f
Byron Tardif <byvinyal@microsoft.com>
2015-03-03 10:47:01 -0800

    Webjob QueueTrigger form appSettings
    Allows the webjob to read the queue name form app settings

--- CQRS/Program.cs
+++ CQRS/Program.cs
@@ -1,72 +1,74 @@
-﻿using Microsoft.Azure.WebJobs;
-using System;
+﻿using System;
+using System.Configuration;
+using Microsoft.Azure.WebJobs;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using System.Configuration;
-
 using GalleryHelpers;
 using System.Data.SqlClient;
 using System.Linq.Expressions;
-
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace CQRS
 {
+
+    //Config Name Resolver is used to allow the use of a Queue name defined in config.
+    internal class ConfigNameResolver : INameResolver
+    {
+        public string Resolve(string name)
+        { 
+            string resolvedName = ConfigurationManager.AppSettings[name]; 
+            if (string.IsNullOrWhiteSpace(resolvedName)) 
+            { 
+                throw new InvalidOperationException("Cannot resolve " + name); 
+            } 
+            return resolvedName; 
+        }
+    }
+
     // To learn more about Microsoft Azure WebJobs, please see http://go.microsoft.com/fwlink/?LinkID=401557
     public class  Program
     {
         static string databaseWrite = "";
         static string databaseRead = "";
         static List<SiteLocation> locations = new List<SiteLocation>();
-        public static JobHostConfiguration config;
 
         static public void Main()
         {
+            JobHostConfiguration hostConfiguration = null;
             var appSettings = System.Configuration.ConfigurationManager.AppSettings;
-            
             var currentLocation = appSettings["siteName"];
+            var queueName = appSettings["queueName"];
             var blob = appSettings["blobContainer"];
             var dbRead = appSettings["dbRead"];
             var dbWrite = appSettings["dbWrite"];
-
-            var queueName = appSettings["queueName"];
-
             var connectionStrings = System.Configuration.ConfigurationManager.ConnectionStrings;
             databaseRead = connectionStrings[dbRead].ConnectionString;
             databaseWrite = connectionStrings[dbWrite].ConnectionString;
-            
+
             Console.WriteLine("currentLocation\t" + currentLocation);
             Console.WriteLine("blob\t" + blob);
-            Console.WriteLine("queue name\t" + queueName);
-            Console.WriteLine("dbRead\t" + dbRead);
-            Console.WriteLine("dbWrite\t" + dbWrite);
+            Console.Write("dbRead\t" + dbRead + "\t");
             Console.WriteLine("databaseRead\t" + databaseRead);
+            Console.Write("dbWrite\t" + dbWrite + "\t");
             Console.WriteLine("databaseWrite\t" + databaseWrite);
-
-
-            Console.WriteLine("Discovering Storage Locations...");
-
+            Console.WriteLine("\n\nDiscovering Storage Locations...");
             for (int i = 0; i < connectionStrings.Count; i++)
             {
-                Console.WriteLine("Connection String Name " + connectionStrings[i].Name);
-                Console.WriteLine("Connection String String " + connectionStrings[i].ConnectionString);
-                Console.WriteLine("Connection String Provider " + connectionStrings[i].ProviderName);
-
                 if (connectionStrings[i].ProviderName == "" && !connectionStrings[i].Name.Contains("Local"))
                 {
                     Console.WriteLine("Adding Storage Location:\t" + connectionStrings[i].Name);
 
                     if (currentLocation == connectionStrings[i].Name)
                     {
-                        config = new JobHostConfiguration(connectionStrings[i].ConnectionString);
-                        config.Queues.MaxPollingInterval = TimeSpan.FromMinutes(0.2);
-                        config.Queues.MaxDequeueCount = 2;
-                        config.Queues.BatchSize = 10;
+                        hostConfiguration = new JobHostConfiguration(connectionStrings[i].ConnectionString){NameResolver = new ConfigNameResolver()};
+                        hostConfiguration.Queues.MaxPollingInterval = TimeSpan.FromSeconds(5);
+                        hostConfiguration.Queues.MaxDequeueCount = 2;
+                        hostConfiguration.Queues.BatchSize = 10;
                     }
                     locations.Add(new SiteLocation(connectionStrings[i].Name, connectionStrings[i].ConnectionString, blob, queueName));
                 }
@@ -75,20 +77,15 @@ namespace CQRS
             if (locations.Count > 0)
             {
                 Console.WriteLine("Total Storage Locations: " + locations.Count);
-                JobHost host = new JobHost(config);
+                JobHost host = new JobHost(hostConfiguration);
                 host.RunAndBlock();
             }
             else 
             {
                 Console.WriteLine("ERROR: No storage locations found!");
             }
-            
-
-
         }
-
-
-        public static void processQueue([QueueTrigger("scaledemo")] Message m)
+        public static void processQueue([QueueTrigger("%queueName%")] Message m)
         {
             Console.WriteLine(m.operation + ": " + m.galleryObject);
             Console.WriteLine(m.serializedobject);
@@ -99,10 +96,9 @@ namespace CQRS
                 v = (Photos)JsonConvert.DeserializeObject(m.serializedobject, typeof(Photos));
                 v.connectionString = databaseWrite;
 
-                if (m.operation == "delete")
-                {
-                    v.delete();
-
+                if (m.operation == "delete") 
+                { 
+                    v.delete(); 
                 }
                 else if (m.operation == "insert")
                 {
